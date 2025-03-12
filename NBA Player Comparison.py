9661020b51285c03acfc76e56fb9d2ffc21332f3
Fernando Soriano <134003926+fjsorianop2023@users.noreply.github.com>
2025-03-12 13:24:30 -0400

    Update NBA Player Comparison.py

--- NBA Player Comparison.py
+++ NBA Player Comparison.py
@@ -1,5 +1,7 @@
-import random  
-
-
-
-def main():
+players = [
+    {"name": "Devin Booker", "height": 1.96, "weight": 93, "playstyle": ["scorer", "playmaker"]},
+    {"name": "Klay Thompson", "height": 1.98, "weight": 97, "playstyle": ["shooter"]},
+    {"name": "Bradley Beal", "height": 1.93, "weight": 94, "playstyle": ["scorer", "playmaker"]},
+    {"name": "Stephen Curry", "height": 1.88, "weight": 84, "playstyle": ["shooter", "playmaker"]},
+    {"name": "Giannis Antetokounmpo", "height": 2.11, "weight": 109, "playstyle": ["slasher", "defender"]},
+]
